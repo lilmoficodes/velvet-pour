be72494765fa95ab5d9833c9ca10416436027c5a
Mafijur <lilmoficodes@gmail.com>
2026-08-26 12:40:22 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger, SplitText);
+const App = () => {
+  return (
+     <>
+     <div>
+      Hello
+     </div>
+     </>
+  )
+}
+
+export default App
